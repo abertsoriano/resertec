320e13961396300f4ae0867a12668cf2934d9d45
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2016-10-19 02:02:54 -0500

    fix position of nav arrows in slick carousel

--- js/tm-scripts.js
+++ js/tm-scripts.js
@@ -697,26 +697,21 @@ if ($slickProductsLogos.length) {
       pauseOnHover: false,
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 1080,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 900,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 767,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 1
           }
         }
         // You can unslick at a given breakpoint now by adding:
